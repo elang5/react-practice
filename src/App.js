daf17fa447dbbea1d75d92fc353e458352634b69
elang5 <green.elan5@gmail.com>
2019-04-23 20:56:56 -0500

    functionality working

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import NavBar from './Components/NavBar';
 import Home from './Components/Home';
 import './App.css';
 import About from './Components/About';
 import Contact from './Components/Contact';
+import Post from './Components/Post';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-      <Route exact path='/' component={Home} />
-      <Route path='/about' component={About} />
-      <Route path='/contact' component={Contact} />
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route path='/about' component={About} />
+        <Route path='/contact' component={Contact} />
+        <Route path='/:post_id' component={Post} />
+      </Switch>
     </div>
   );
 }
